eb7472af5c3523589540cc38548294cd4f569814
Veronica B <veronicabreguta@yahoo.com>
2024-02-06 19:23:24 -0600

    Created getPlayerChoice and playRound functions

--- script.js
+++ script.js
@@ -21,3 +21,43 @@ function getComputerChoice() {
     }
 
 }
+
+function getPlayerChoice() {
+
+    let userInput = prompt('Choose: "Rock" "Paper" "Scissors"');
+
+    if (userInput.toLowerCase() === 'rock') {
+        return 'Rock';
+    } else if (userInput.toLowerCase() === 'paper') {
+        return 'Paper';
+    } else if (userInput.toLowerCase() === 'scissors') {
+        return 'Scissors';
+    }
+
+}
+
+
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return 'Same choice. Let\'s try again';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You won. Rock beats scissors';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock'){
+        return 'You lost. Rock beats scissors';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return 'You won. Scissors beat paper';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return 'You lost. Scissors beat paper';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
+        return 'You won. Paper beats rock';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return 'You lost. Paper beats rock';
+    }
+
+}
+
